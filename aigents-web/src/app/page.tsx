0b07167de69249909df5f95cfac77bce28879456
jerome.cousteau <jerome.cousteau@gmail.com>
2025-06-07 23:48:37 -0600

    Setup background

--- aigents-web/src/app/page.tsx
+++ aigents-web/src/app/page.tsx
@@ -2,19 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#10142A]">
-      <main className="flex flex-col items-center justify-center">
-        <Image
-          src="/AIGents-logo-highQ.png"
-          alt="AIGents Logo"
-          width={500}
-          height={150}
-          priority
-        />
-        <p className="text-xl text-white mt-4">
-          Best Customer Service. Every time.
-        </p>
-      </main>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <Image
+        src="/AIGents-logo-no-background.png"
+        alt="AIGents Logo"
+        width={600}
+        height={230}
+        priority
+      />
+    </main>
   );
 }
