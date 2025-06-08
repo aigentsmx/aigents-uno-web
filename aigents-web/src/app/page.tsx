7f8b6d87104e913e816a7b514e0db6d386cbcb93
jerome.cousteau <jerome.cousteau@gmail.com>
2025-06-08 10:57:35 -0600

    chore: use correct contact info

--- aigents-web/src/app/page.tsx
+++ aigents-web/src/app/page.tsx
@@ -148,11 +148,11 @@ export default function Home() {
                 <div className="space-y-6">
                   <div className="flex items-center space-x-4">
                     <Mail className="h-6 w-6 text-[#C75AF6]" />
-                    <span className="text-gray-300">contacto@aigents.com</span>
+                    <span className="text-gray-300">contacto@aigents.uno</span>
                   </div>
                   <div className="flex items-center space-x-4">
                     <Phone className="h-6 w-6 text-[#F55AFC]" />
-                    <span className="text-gray-300">+1 (555) 123-4567</span>
+                    <span className="text-gray-300">+52 777 2068414</span>
                   </div>
                   <div className="flex items-center space-x-4">
                     <MapPin className="h-6 w-6 text-[#f6e6c3]" />
